a501826d73deda687ac7690670e372caf4de2094
Adilet Momunaliev, Nina Potrebich and Wei Dai <wei@>
2019-10-29 10:33:59 -0700

    Creating web page with for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
-$(document).ready(function() {
+$(document).ready(function()
+{
 
+  $("form#formID").submit(function(event)
+  {
+    event.preventDefault();
+    var string = $("#puzzle").val();
 
+    var newString="";
+    for(var index=0; index<string.length;index++)
+    {
+      if( string[index]!='a' && string.charAt(index)!='e'&&
+          string.charAt(index)!='i'&& string.charAt(index)!='o'&&
+          string.charAt(index)!='u'
+        )
+        {
+          newString+=string.charAt(index);
+          console.log(string.charAt(index));
+        }
+        else{
+          newString=newString+"-";
+        }
+
+    }
+
+    $("#output").text(newString).show();
+    $("form").hide();
+
+  });
 
 });
